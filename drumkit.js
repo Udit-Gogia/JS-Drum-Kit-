819b40b36033192669f999dcaf6bd71e9564d842
Gogia_Udit_17 <88316529+Udit-Gogia@users.noreply.github.com>
2022-01-06 18:15:07 +0530

    caps lock on enabled

--- drumkit.js
+++ drumkit.js
@@ -14,46 +14,55 @@ function playsound(key){
 
     switch (key){
         case "a":
+        case "A":
             const a = new Audio('clap.wav');
             a.play();
             break;
 
         case "s":
+        case "S":
             const s = new Audio('hihat.wav');
             s.play();
             break;
 
         case "d":
+        case "D":
             const d = new Audio('kick.wav');
             d.play();
             break;
 
         case "f":
+        case "F":
             const f = new Audio('openhat.wav');
             f.play();
             break;
 
         case "g":
+        case "G":
             const g = new Audio('boom.wav');
             g.play();
             break;
 
         case "h":
+        case "H":
             const h = new Audio('ride.wav');
             h.play();
             break;
 
         case "j":
+        case "J":
             const j = new Audio('snare.wav');
             j.play();
             break;
 
         case "k":
+        case "K":
             const k = new Audio('tom.wav');
             k.play();
             break;
 
         case "l":
+        case "L":
             const l = new Audio('tink.wav');
             l.play();
             break;
