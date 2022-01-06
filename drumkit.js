a61280cb0ce822ffb29db3a311fa4417c929f646
Gogia_Udit_17 <88316529+Udit-Gogia@users.noreply.github.com>
2022-01-06 17:56:10 +0530

    project completed

--- drumkit.js
+++ drumkit.js
@@ -4,53 +4,57 @@ window.addEventListener("keydown", function (e){
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     key.classList.add('playing');
 
+    setTimeout( function (){
+        key.classList.remove('playing')
+    },100);
+
 });
 
 function playsound(key){
 
     switch (key){
         case "a":
-            const a = new Audio('sounds/clap.wav');
+            const a = new Audio('clap.wav');
             a.play();
             break;
 
         case "s":
-            const s = new Audio('sounds/hihat.wav');
+            const s = new Audio('hihat.wav');
             s.play();
             break;
 
         case "d":
-            const d = new Audio('sounds/kick.wav');
+            const d = new Audio('kick.wav');
             d.play();
             break;
 
         case "f":
-            const f = new Audio('sounds/openhat.wav');
+            const f = new Audio('openhat.wav');
             f.play();
             break;
 
         case "g":
-            const g = new Audio('sounds/boom.wav');
+            const g = new Audio('boom.wav');
             g.play();
             break;
 
         case "h":
-            const h = new Audio('sounds/ride.wav');
+            const h = new Audio('ride.wav');
             h.play();
             break;
 
         case "j":
-            const j = new Audio('sounds/snare.wav');
+            const j = new Audio('snare.wav');
             j.play();
             break;
 
         case "k":
-            const k = new Audio('sounds/tom.wav');
+            const k = new Audio('tom.wav');
             k.play();
             break;
 
         case "l":
-            const l = new Audio('sounds/tink.wav');
+            const l = new Audio('tink.wav');
             l.play();
             break;
     }
