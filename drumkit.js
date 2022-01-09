6ee784a615f13e3ce2890fe3c3bc7e89398f12a9
Gogia_Udit_17 <88316529+Udit-Gogia@users.noreply.github.com>
2022-01-09 22:14:19 +0530

    changes in responsiveness

--- drumkit.js
+++ drumkit.js
@@ -15,55 +15,55 @@ function playsound(key){
     switch (key){
         case "a":
         case "A":
-            const a = new Audio('clap.wav');
+            const a = new Audio('sounds/clap.wav');
             a.play();
             break;
 
         case "s":
         case "S":
-            const s = new Audio('hihat.wav');
+            const s = new Audio('sounds/hihat.wav');
             s.play();
             break;
 
         case "d":
         case "D":
-            const d = new Audio('kick.wav');
+            const d = new Audio('sounds/kick.wav');
             d.play();
             break;
 
         case "f":
         case "F":
-            const f = new Audio('openhat.wav');
+            const f = new Audio('sounds/openhat.wav');
             f.play();
             break;
 
         case "g":
         case "G":
-            const g = new Audio('boom.wav');
+            const g = new Audio('sounds/boom.wav');
             g.play();
             break;
 
         case "h":
         case "H":
-            const h = new Audio('ride.wav');
+            const h = new Audio('sounds/ride.wav');
             h.play();
             break;
 
         case "j":
         case "J":
-            const j = new Audio('snare.wav');
+            const j = new Audio('sounds/snare.wav');
             j.play();
             break;
 
         case "k":
         case "K":
-            const k = new Audio('tom.wav');
+            const k = new Audio('sounds/tom.wav');
             k.play();
             break;
 
         case "l":
         case "L":
-            const l = new Audio('tink.wav');
+            const l = new Audio('sounds/tink.wav');
             l.play();
             break;
     }
